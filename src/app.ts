be67abceadff20c6713b01173c449353ba902e2d
Afik Habaz <afik762@gmail.com>
2023-03-06 00:59:28 +0200

    user routes file created and imported

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import 'dotenv/config';
 import express from 'express';
 
+import userRoutes from './routes/user.route';
+
 const app = express();
 
-
+app.use('/user', userRoutes);
 
 app.listen(process.env.PORT, () => console.log(`App is running on port: ${process.env.PORT}`));
---
+++ src/routes/user.route.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const router = express.Router();
+
+// create new user
+
+// update user by id
+
+// get all users
+
+// get User by id
+
+// delete user by id
+
+export default router;
